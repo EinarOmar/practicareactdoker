a092c5968d5abb34f87fd8b5787eb922e3b53837
Einar Omar <Kiritoruizeo@gmail.com>
2023-10-27 11:06:38 -0600

    pruebas unitarias 2

---
+++ tests/unitariaRenderizaion.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../src/App";
+
+test("Prueba Unitaria 1: Verificar que el componente se renderice correctamente", () => {
+  render(<App />);
+  
+  // Verificar que el título esté presente en la página
+  expect(screen.getByText("Claculadora de divisas")).toBeInTheDocument();
+
+  // Verificar que el campo de entrada esté presente
+  expect(screen.getByPlaceholderText("Introduce tu dinero MXN")).toBeInTheDocument();
+
+  // Verificar que el botón de conversión esté presente
+  expect(screen.getByText("CONVERTIR")).toBeInTheDocument();
+});
